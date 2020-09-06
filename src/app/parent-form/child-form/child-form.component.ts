e223462f96ba9f9c2afc01128f29e38c51426873
virajdicholkar <dicholkar.vv@gmail.com>
2020-09-06 17:19:38 +0530

    Added value accessor for child form component

--- src/app/parent-form/child-form/child-form.component.ts
+++ src/app/parent-form/child-form/child-form.component.ts
@@ -1,10 +1,55 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, forwardRef, OnInit } from '@angular/core';
+import {
+  NG_VALUE_ACCESSOR,
+  FormGroup,
+  FormBuilder,
+  FormArray,
+  Validators,
+} from '@angular/forms';
+const PERSON_VALUE_ACCESSOR: any = {
+  provide: NG_VALUE_ACCESSOR,
+  // tslint:disable-next-line: no-use-before-declare
+  useExisting: forwardRef(() => ChildFormComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'child-form',
   templateUrl: './child-form.component.html',
-  styleUrls: [ './child-form.component.css' ]
+  styleUrls: [ './child-form.component.css' ],
+  providers: [PERSON_VALUE_ACCESSOR],
 })
-export class ChildFormComponent  {
-  name = 'Angular ' + VERSION.major;
+export class ChildFormComponent implements OnInit {
+  
+  onChange: (value) => {};
+  onTouched: () => {};
+  personForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+  ngOnInit(){
+    this.personForm = this.formBuilder.group({
+      firstName: '',
+      lastName: ''
+    })
+  }
+
+  //cva methods
+    // CVA method
+  writeValue(obj: any): void {
+    this.personForm.patchValue(obj);
+  }
+
+  // CVA method
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+    this.personForm.valueChanges.subscribe((data) => {
+      fn(data);
+    });
+  }
+
+  // CVA method
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
 }

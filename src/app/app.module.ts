e18f5117536a758c0e98a22e8d46f8bc9dfb3902
virajdicholkar <dicholkar.vv@gmail.com>
2020-09-06 17:05:27 +0530

    Added initial form values and methods for parent form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -8,13 +8,17 @@ import { ParentFormComponent } from './parent-form/parent-form.component';
 import { ChildFormComponent } from './parent-form/child-form/child-form.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    ReactiveFormsModule
+    ],
   declarations: [ 
     AppComponent, 
     HelloComponent,
     ParentFormComponent,
     ChildFormComponent
     ],
-  bootstrap:    [ AppComponent ]
+  bootstrap:[ AppComponent ]
 })
 export class AppModule { }
--- src/app/parent-form/parent-form.component.ts
+++ src/app/parent-form/parent-form.component.ts
@@ -1,10 +1,46 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'parent-form',
   templateUrl: './parent-form.component.html',
   styleUrls: [ './parent-form.component.css' ]
 })
-export class ParentFormComponent  {
+export class ParentFormComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+  parentForm: FormGroup;
+  newPerson = {}
+  personList = [
+    {
+      firstName: 'Micky',
+      lastName: 'Mouse'
+    }
+  ]
+  constructor(
+    private formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(){
+    this.parentForm = this.formBuilder.group({
+      personList: this.formBuilder.array([])
+    });
+
+    this.personList.forEach((person)=>this.addToList(person));
+  }
+
+  get f(){
+    return this.parentForm.controls; 
+  }
+
+  addToList(data){
+    const list = this.f['personList'] as FormArray;
+    const listItem = this.formBuilder.control(data);
+    list.push(listItem);
+    this.newPerson = {};
+  }
+
+  removeFromList(index){
+    const list = this.f['personList'] as FormArray;
+    list.removeAt(index);
+  }
 }
